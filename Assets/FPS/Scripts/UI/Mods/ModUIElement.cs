714b62cf7b3b2e53bf5db47c8681f8b6eee3887d
Jonas Valstar <jonasvalstar@gmail.com>
2025-03-25 12:35:33 +0100

    Small Changes

--- Assets/FPS/Scripts/UI/Mods/ModUIElement.cs
+++ Assets/FPS/Scripts/UI/Mods/ModUIElement.cs
@@ -66,16 +66,16 @@ public class ModUIElement : MonoBehaviour
                 case ModType.statChange:
                     description = $"{description}changed {effect.statType} by {effect.value}";
                     switch (effect.statType) { // getting the end value
-                        case StatType.critChance: description = "%\n"; break; // Percent crit chance
-                        case StatType.fireDelay: description = " sec\n"; break; // seconds fire delay
-                        case StatType.spreadAngle: description = " deg\n"; break; // degree angle of spread
-                        case StatType.bulletsPerShot: description = " bullets\n"; break; // bullets per shot
-                        case StatType.recoilForce: description = " N\n"; break; // Newton of force to recoil
-                        case StatType.maxAmmo: description = " shots\n"; break; // shots before empty
-                        case StatType.reloadSpeed: description = " shots/sec\n"; break; // shots that the charge reloads per second
-                        case StatType.reloadDelay: description = " sec\n"; break; // seconds before reload start
-                        case StatType.bulletVel: description = " m/s\n"; break; // meter per second that the bullet starts with
-                        case StatType.bulletAcc: description = " m/s^2\n"; break; // meter per second squared that the bullet accelerates
+                        case StatType.critChance: description = $"{description}%\n"; break; // Percent crit chance
+                        case StatType.fireDelay: description = $"{description} sec\n"; break; // seconds fire delay
+                        case StatType.spreadAngle: description = $"{description} deg\n"; break; // degree angle of spread
+                        case StatType.bulletsPerShot: description = $"{description} bullets\n"; break; // bullets per shot
+                        case StatType.recoilForce: description = $"{description} N\n"; break; // Newton of force to recoil
+                        case StatType.maxAmmo: description = $"{description} shots\n"; break; // shots before empty
+                        case StatType.reloadSpeed: description = $"{description} shots/sec\n"; break; // shots that the charge reloads per second
+                        case StatType.reloadDelay: description = $"{description} sec\n"; break; // seconds before reload start
+                        case StatType.bulletVel: description = $"{description} m/s\n"; break; // meter per second that the bullet starts with
+                        case StatType.bulletAcc: description = $"{description} m/s^2\n"; break; // meter per second squared that the bullet accelerates
                     }
                     break;
 
